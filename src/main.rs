64ae8498170746b7bc22fc73ea6050c4c3ab9b57
Rxdgxn <radugantoi@gmail.com>
2023-07-13 19:38:57 +0300

    Added nested conditions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // #![allow(dead_code, mutable_transmutes, non_camel_case_types, non_snake_case, non_upper_case_globals, unused_assignments, unused_mut, unused_variables)]
 use std::collections::HashMap;
+use std::slice::Iter;
 use Token::*;
 use Operator::*;
 use Value::*;
@@ -69,6 +70,44 @@ fn token_to_string(tok: &Token) -> Option<&String> {
     }
 }
 
+fn return_instr(w: &str, rpnstack: &Vec<Token>, it: &mut Iter<'_, Token>) -> Option<Instruction> {
+    match w {
+        "PRINT" => Some(PRINT(rpnstack[1..rpnstack.len()].to_vec())),
+        "GOTO" => Some(GOTO((&rpnstack[1]).clone())),
+        "BG" => {
+            let mut tmpit = it.clone();
+            let mut tmp = tmpit.next();
+            let mut idx = 1usize;
+            while let Some(t) = tmp {
+                if token_to_string(t) != None {
+                    if KEYWORDS.contains(&(&token_to_string(t).unwrap() as &str)) {
+                        return Some(BG(rpnstack[1..idx].to_vec(), Box::new(return_instr(&token_to_string(t).unwrap(), &rpnstack[idx..rpnstack.len()].to_vec(), &mut tmpit).unwrap())));
+                    }
+                }
+                tmp = tmpit.next();
+                idx += 1;
+            }
+            return None;
+        }
+        "BZ" => {
+            let mut tmpit = it.clone();
+            let mut tmp = tmpit.next();
+            let mut idx = 1usize;
+            while let Some(t) = tmp {
+                if token_to_string(t) != None {
+                    if KEYWORDS.contains(&(&token_to_string(t).unwrap() as &str)) {
+                        return Some(BZ(rpnstack[1..idx].to_vec(), Box::new(return_instr(&token_to_string(t).unwrap(), &rpnstack[idx..rpnstack.len()].to_vec(), &mut tmpit).unwrap())));
+                    }
+                }
+                tmp = tmpit.next();
+                idx += 1;
+            }
+            return None;
+        }
+        _ => panic!("Something went wrong")
+    }
+}
+
 fn evalrpn(rpnstack: &Vec<Token>, vars: &HashMap<String, i32>, lc: usize, program: &mut Vec<Instruction>, labels: &HashMap<String, usize>) -> i32 {
     let mut numstack: Vec<i32> = Vec::new();
 
@@ -84,49 +123,8 @@ fn evalrpn(rpnstack: &Vec<Token>, vars: &HashMap<String, i32>, lc: usize, progra
                         true => numstack.push(vars[w]),
                         false => {
                             if KEYWORDS.contains(&(w as &str)) {
-                                match w as &str {
-                                    // The 1 index should be valid when the rpnstack is passed to the instruction
-                                    "PRINT" => program.push(PRINT(rpnstack[1..rpnstack.len()].to_vec())),
-                                    "GOTO" => program.push(GOTO((&rpnstack[1]).clone())),
-                                    "BG" => {
-                                        let mut tmpit = it.clone();
-                                        let mut tmp = tmpit.next();
-                                        let mut idx = 1usize;
-                                        while let Some(t) = tmp {
-                                            if token_to_string(t) != None {
-                                                if KEYWORDS.contains(&(&token_to_string(t).unwrap() as &str)) {
-                                                    program.push(BG(rpnstack[1..idx].to_vec(), Box::new(match &token_to_string(t).unwrap() as &str {
-                                                        "PRINT" => PRINT(rpnstack[idx+1..rpnstack.len()].to_vec()),
-                                                        "GOTO" => GOTO((&rpnstack[idx+1]).clone()),
-                                                        _ => panic!("Not implemented yet (recursion for nested instructions)")
-                                                    })));
-                                                }
-                                            }
-                                            tmp = tmpit.next();
-                                            idx += 1;
-                                        }
-                                    }
-                                    "BZ" => {
-                                        let mut tmpit = it.clone();
-                                        let mut tmp = tmpit.next();
-                                        let mut idx = 1usize;
-                                        while let Some(t) = tmp {
-                                            if token_to_string(t) != None {
-                                                if KEYWORDS.contains(&(&token_to_string(t).unwrap() as &str)) {
-                                                    program.push(BZ(rpnstack[1..idx].to_vec(), Box::new(match &token_to_string(t).unwrap() as &str {
-                                                        "PRINT" => PRINT(rpnstack[idx+1..rpnstack.len()].to_vec()),
-                                                        "GOTO" => GOTO((&rpnstack[idx+1]).clone()),
-                                                        _ => panic!("Not implemented yet (recursion for nested instructions)")
-                                                    })));
-                                                }
-                                            }
-                                            tmp = tmpit.next();
-                                            idx += 1;
-                                        }
-                                    }
-                                    _ => panic!("Something went wrong")
-                                }
-                                break; // ?
+                                program.push(return_instr(w as &str, rpnstack, &mut it).unwrap());
+                                break;
                             }
                             else if labels.contains_key(w) {
                                 // This should never happen
@@ -180,7 +178,6 @@ fn evalprogram(program: &mut Vec<Instruction>, vars: &mut HashMap<String, i32>, 
                     let mut p = Vec::new();
                     p.push(*instr.clone());
                     idx = evalprogram(&mut p, vars, labels);
-                    continue;
                 }
             }
             BZ(rpnstack, instr) => {
@@ -188,7 +185,6 @@ fn evalprogram(program: &mut Vec<Instruction>, vars: &mut HashMap<String, i32>, 
                     let mut p = Vec::new();
                     p.push(*instr.clone());
                     idx = evalprogram(&mut p, vars, labels);
-                    continue;
                 }
             }
             LABEL(l, i) => _ = labels.insert(l.clone(), *i),
